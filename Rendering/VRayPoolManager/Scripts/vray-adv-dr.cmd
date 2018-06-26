c8aa076f8f504aee97fd81c742e1d8dc2bb24b8d
Christian Smith <christis@microsoft.com>
2018-06-26 11:58:30 +1000

    Fix comment in batch file.

--- Rendering/VRayPoolManager/Scripts/vray-adv-dr.cmd
+++ Rendering/VRayPoolManager/Scripts/vray-adv-dr.cmd
@@ -5,7 +5,7 @@ echo [Directories]> "C:\Autodesk\3dsMax2018\vrayspawner.ini"
 echo AppName=C:\Autodesk\3dsMax2018\3dsmaxio.exe>> "C:\Autodesk\3dsMax2018\vrayspawner.ini"
 
 rem Install Backburner if available.
-#start /wait msiexec /i Backburner.msi /qn
+rem start /wait msiexec /i Backburner.msi /qn
 
 rem start vray spawner
 start /wait "vrayspawner" "C:\Autodesk\3dsMax2018\vrayspawner2018.exe" "-port=%port%"
